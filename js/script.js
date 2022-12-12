fc9c1d3dac31eee231bc180aa0ca6abd061521b8
Adam Musial <adammusial@wp.pl>
2022-12-12 16:43:21 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,18 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -18,37 +30,42 @@
                 `<li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}
             >
+            <button class="js-remove">usuń</button>
             ${task.content}
             </li >`;
-        }
+        };
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
+
     const onFormSubmit = (event) => {
-            event.preventDefault();
+        event.preventDefault();
 
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-            if (newTaskContent === "") {
-                return;
-            }
-            addNewTask(newTaskContent);
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
 
     }
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
 
-        render();
-    }
+
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", onFormSubmit)
-        };
-    
+    };
+
 
     init();
 }
