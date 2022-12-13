87cfa267dd545a876ab11f2eb88992a3f8519212
Adam Musial <adammusial@wp.pl>
2022-12-13 10:11:55 +0100

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -25,23 +25,9 @@
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
-    }
+    };
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString +=
-                `<li
-            ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-            >
-            <button class="js-done">zrobione?</button>
-            <button class="js-remove">usuń</button>
-            ${task.content}
-            </li >`;
-        };
-        document.querySelector(".js-tasks").innerHTML = htmlString;
- 
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -57,6 +43,25 @@
                 toggleTaskDone(index);
             });
         });
+    }
+
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString +=
+                `<li
+            ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+            >
+            <button class="js-done">zrobione?</button>
+            <button class="js-remove">usuń</button>
+            ${task.content}
+            </li >`;
+        };
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
 
